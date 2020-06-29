57ede15603c60e4f8131427748d3e34a2c6cbaba
LittleControl <littlecontrol2019@gmail.com>
2020-06-29 22:28:04 +0800

    商家店铺页面路由

---
+++ src/components/ShopHead.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <p>ShopHead</p>
+  </div>
+</template>
+<script>
+ export default {
+ }
+</script>
+<style scoped lang="stylus">
+ 
+</style>
---
+++ src/pages/ShopInfo/ShopInfo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Evaluation />
+    <Food />
+    <Abort />
+  </div>
+</template>
+<script>
+    import Evaluation from './Evaluation'
+    import Food from './Food'
+    import Abort from './Abort'
+
+    export default {
+        components: {
+            Evaluation,
+            Food,
+            Abort
+        }
+    }
+</script>
+<style scoped lang="stylus">
+ 
+</style>
